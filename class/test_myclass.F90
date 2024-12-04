99ed97405d89a21eba8c106b856f5fa5e1b616ff
Alexander Pletzer <alexander@gokliya.net>
2024-12-05 09:50:23 +1300

    ifdefs for print the address

--- class/test_myclass.F90
+++ class/test_myclass.F90
@@ -28,7 +28,11 @@ contains
         integer, intent(in)                 :: n
         allocate(this%arr(n))
         ref_count = ref_count + 1
+#ifdef HAS_LOC
         print *, 'constructor was called ', ref_count, ' object address: ', %LOC(this)
+#else
+        print *, 'constructor was called ', ref_count
+#endif
     end function myclass_new
 
     subroutine myclass_set(this, vals)
@@ -46,7 +50,11 @@ contains
         ! some compilers have already deallocated this%arr
         deallocate(this%arr, stat=ier)
         ref_count = ref_count - 1
+#ifdef HAS_LOC
         print *, 'destructor was called ', ref_count, ' object address: ', %LOC(this)
+#else
+        print *, 'destructor was called ', ref_count
+#endif
     end subroutine myclass_del
 
 end module myclass_mod
